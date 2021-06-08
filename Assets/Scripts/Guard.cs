c23f1b9c9f31323590f603fd791902f03d4f76d4
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-06-08 09:45:52 +0600

    turning debugged

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -41,7 +41,7 @@ public class Guard : MonoBehaviour {
 
     IEnumerator TurnToFace (Vector3 lookTarget) {
         Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
-        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Deg2Rad;
+        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
 
         while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f) {
             float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
